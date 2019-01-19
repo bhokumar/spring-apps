d361832884f1645c7998d6660ba7fe860330955a
Bhoopendra Kumar <bhoopendra.akgec@gmail.com>
2019-01-19 11:57:57 +0530

    Adding jms-ibm-mq project

---
+++ jms-ibm-mq/src/main/java/org/smartapps/jms/jmsclientibmmq/JmsClientIbmMqApplication.java
@@ -0,0 +1,12 @@
+package org.smartapps.jms.jmsclientibmmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsClientIbmMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsClientIbmMqApplication.class, args);
+	}
+}
