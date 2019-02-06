795eed6c9c561f04b6331d6e4fd228ebef9cc65e
Bhoopendra Kumar <bhoopendra.akgec@gmail.com>
2019-02-06 07:41:46 +0530

    Adding webflux project

---
+++ webflux-app/src/main/java/org/webflux/webfluxapp/WebfluxAppApplication.java
@@ -0,0 +1,14 @@
+package org.webflux.webfluxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxAppApplication.class, args);
+	}
+
+}
+
---
+++ webflux-app/src/main/java/org/webflux/webfluxapp/controllers/EmployeeController.java
@@ -0,0 +1,14 @@
+package org.webflux.webfluxapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.webflux.webfluxapp.bean.Employee;
+
+@RestController
+public class EmployeeController {
+
+    @GetMapping("/employees")
+    public Employee getEmployee(){
+        return new Employee("1000", "Bhoopendra");
+    }
+}
